2900ded5446385a663b3129078d6ad9400965da7
JordanComas <jordancomas1@yahoo.com>
2022-08-02 17:15:32 -0400

    push to heroku

--- app.js
+++ app.js
@@ -46,10 +46,17 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("mongodb://localhost/final-project")
+  .connect(process.env.MONGODB_URI)
   .then((x) =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
   .catch((err) => console.error("Error connecting to mongo", err));
+
+// mongoose
+//   .connect("mongodb://localhost/final-project")
+//   .then((x) =>
+//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+//   )
+//   .catch((err) => console.error("Error connecting to mongo", err));
 
 module.exports = app;
